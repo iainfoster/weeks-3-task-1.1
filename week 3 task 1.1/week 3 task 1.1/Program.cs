4df6e3350b92f6afd35ad28072a61edc24c91670
Iain Foster <9925082@student.toiohomai.ac.nz>
2018-08-15 16:44:52 +1200

    task 1.1

---
+++ week 3 task 1.1/week 3 task 1.1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_3_task_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("HELLO, can you guess my favorite: ");
+            string Pet = (Console.ReadLine());
+            switch (Pet)
+            {
+                case "sheep":
+                case "cat":
+                case "cow":
+                    Console.WriteLine("Na");
+                    break;
+                case "dog":
+                    Console.WriteLine("yea");
+                    break;
+                default:
+                    Console.WriteLine("hmm thats not a pet");
+                    break;
+
+            }
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+    }
+}
